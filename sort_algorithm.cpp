8f809025968d9c5ee816dd85e5aa00e0afacb245
xhw <xuhw16@mails.tsinghua.edu.cn>
2018-08-07 17:00:08 +0800

    sort algorithm

--- sort_algorithm.cpp
+++ sort_algorithm.cpp
@@ -3,31 +3,91 @@
 using namespace std;
 /*
 input:  n //size of sorted numbers
-		[x1,x2,x3..xn]//vector to be sort 
+[x1,x2,x3..xn]//vector to be sort
 sort algorithm:
-		bubble sort: o(n^2),stable sort.
-
+bubble sort: o(n^2),stable sort.
+insert sort: o(n^2),stable sort.
+merge sort:o(nlog(n)),distable sort.
 */
-void bubble_sort( vector<int>&result) {
+//bubble sort
+void bubble_sort(vector<int>&result) {
 	int nums = result.size();
 	for (int i = 0; i < nums - 1; i++) {
-		for (int j = 0; j < nums - i-1; j++) {
+		for (int j = 0; j < nums - i - 1; j++) {
 			if (result[j] > result[j + 1])
-				swap(result[j],result[j + 1]);
+				swap(result[j], result[j + 1]);
 		}
 	}
 
 }
 
+//insert sort
+void insert_sort(vector<int> &result) {
+	int nums = result.size();
+	for (int i = 1; i < nums; i++) {
+		int tmp = result[i];
+		int j = i - 1;
+		while (j > 0 && tmp < result[j]) {
+			result[j + 1] = result[j];
+			--j;
+		}
+		result[j + 1] = tmp;
+	}
+}
+
+
+//merge sort
+void merge_all(vector<int>&result, vector<int>left_re, vector<int>right_re) {
+	int n = result.size();
+	int l = left_re.size();
+	int r = right_re.size();
+	int lr = 0, lf = 0;
+	for (int i = 0; i < n; i++) {	
+		while (lr < r&&lf < l) {
+			if (left_re[lf] <= right_re[lr]) {
+				result[i] = left_re[lf];
+				++lf;
+				++i;
+			}
+			else {
+				result[i] = right_re[lr];
+				++lr;
+				++i;
+			}
+
+		}
+		while (lr < r) {
+			result[i] = right_re[lr];
+			++lr;
+			++i;
+		}
+		while(lf<l){
+			result[i] = left_re[lf];
+			++lf;
+			++i;
+		}
+	}
+}
+void merge_sort(vector<int>&result) {
+	int nums = result.size();
+	if (nums == 1)return;
+	vector<int>left_re(result.begin(), result.begin() + nums / 2);
+	vector<int>right_re(result.begin() + nums / 2,result.end());
+	merge_sort(left_re);
+	merge_sort(right_re);
+	merge_all(result, left_re, right_re);
+}
+
 int main() {
 	//input
-	int n=0;
+	int n = 0;
 	cin >> n;
-	vector<int>result(n,0);
+	vector<int>result(n, 0);
 	for (int i = 0; i < result.size(); i++)
 		cin >> result[i];
-
-	bubble_sort(result);
+	merge_sort(result);
+	//insert_sort(result);
+	//bubble_sort(result);
 
 	//output
 	for (int i = 0; i < result.size(); i++)
